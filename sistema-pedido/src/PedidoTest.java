dd1809e2755c53ddf87c6c12a1020e620adf42ad
ArthurOcg <arthur.lima.oliveira@ccc.ufcg.edu.br>
2016-11-23 21:02:45 -0300

    Criando a classe Pedido através do Teste

--- sistema-pedido/src/PedidoTest.java
+++ sistema-pedido/src/PedidoTest.java
@@ -1,12 +1,14 @@
-import static org.junit.Assert.*;
-
 import org.junit.Before;
 import org.junit.Test;
 
 public class PedidoTest {
+	
+	Pedido novoPedido;
 
 	@Before
 	public void setUp() throws Exception {
+		novoPedido = new Pedido();
+		
 	}
 
 	@Test
